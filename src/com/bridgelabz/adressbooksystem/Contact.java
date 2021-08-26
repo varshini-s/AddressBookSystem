83b3633f4addec5819e0f03dab8a0349a12651b8
varshini <dreamvarshini@gmail.com>
2021-08-26 22:44:36 +0530

    ADDED:defined contacts class

---
+++ src/com/bridgelabz/adressbooksystem/Contact.java
@@ -0,0 +1,95 @@
+package com.bridgelabz.adressbooksystem;
+
+public class Contact 
+
+{
+	private String firstName,lastName,address,city,state,zip,phoneNumber,email;
+	
+	public Contact()
+	{
+		
+	}
+	public Contact(String firstName,String lastName,String address,String city,String state,String zip,String phoneNumber,String email)
+	{
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phoneNumber=phoneNumber;
+		this.email=email;
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	public String getCity()
+	{
+		return city;
+	}
+	public String getState()
+	{
+		return state;
+	}
+	public String getZip()
+	{
+		return zip;
+	}
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	
+	//set methods
+	
+	public void setFirstName(String firstName)
+	{
+		 this.firstName=firstName;
+	}
+	public void setLastName(String lastName)
+	{
+		this.lastName=lastName;
+	}
+	public void setAddress(String address)
+	{
+		this.address=address;
+	}
+	public void setCity(String city)
+	{
+		this.city=city;
+	}
+	public void setState(String state)
+	{
+		this.state=state;
+	}
+	public void setZip(String zip)
+	{
+		this.zip=zip;
+	}
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber=phoneNumber;
+	}
+	public void setEmail(String email)
+	{
+		this.email=email;
+	}
+	
+	
+
+}
