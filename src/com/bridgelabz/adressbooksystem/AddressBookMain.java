407e2c14ea6628585a020a152be18b9cd81cb0b7
varshini <dreamvarshini@gmail.com>
2021-08-26 22:38:46 +0530

    ADDED:printing welcome message

---
+++ src/com/bridgelabz/adressbooksystem/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.adressbooksystem;
+
+import java.util.Scanner;
+
+public class AddressBookMain 
+{
+
+		public static void main(String args[]) 
+		{
+			System.out.println("Welcome to Address book system program");
+
+			
+		}
+}
