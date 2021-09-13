c0abab3e7968ed013a8754708e00767d3a841feb
varshini <dreamvarshini@gmail.com>
2021-09-13 16:04:36 +0530

    ADDED: adressbook system class to maintain list of addressbooks

---
+++ src/com/bridgelabz/adressbooksystem/AddressBookSystem.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.adressbooksystem;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AddressBookSystem 
+{
+	
+	private List<AddressBook> addressbookList = new LinkedList<AddressBook>();
+
+	public List<AddressBook> getAddressbookList() 
+	{
+		return addressbookList;
+	}
+
+	public void setAddressbookList(List<AddressBook> addressbookList) 
+	{
+		this.addressbookList = addressbookList;
+	}
+	
+	
+
+}
