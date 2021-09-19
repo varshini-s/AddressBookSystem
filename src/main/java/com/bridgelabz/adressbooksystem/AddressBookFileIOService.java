373734e4b6504cda801037f31a5e45bfd3b74011
varshini <dreamvarshini@gmail.com>
2021-09-19 22:51:35 +0530

    ADDED: function to read from file to list

--- src/main/java/com/bridgelabz/adressbooksystem/AddressBookFileIOService.java
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBookFileIOService.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBookFileIOService 
@@ -58,4 +59,39 @@ public class AddressBookFileIOService
 		return entries;
 	}
 
+	
+	public List<Contact> readData()
+	{
+		List<Contact> contactList = new ArrayList<Contact>();
+		
+		List<String[]> contactDataFromFile = new ArrayList<String[]>();
+		try
+		{
+			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath())
+				.map(line->line.trim())
+				.forEach(line->contactDataFromFile.add(line.split(",")));
+			
+			for(int index=0;index<contactDataFromFile.size();index++)
+			{
+				
+				contactList.add(new Contact(contactDataFromFile.get(index)[0].split("=")[1]
+										,contactDataFromFile.get(index)[1].split("=")[1]
+										,contactDataFromFile.get(index)[2].split("=")[1]
+										,contactDataFromFile.get(index)[3].split("=")[1]
+										,contactDataFromFile.get(index)[4].split("=")[1]
+										,contactDataFromFile.get(index)[5].split("=")[1]
+										,contactDataFromFile.get(index)[6].split("=")[1]
+										,contactDataFromFile.get(index)[7].split("=")[1]
+										));
+			}
+			
+			
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return contactList;
+	}
 }
