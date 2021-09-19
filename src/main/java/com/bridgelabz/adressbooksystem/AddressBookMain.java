c4528ef44652766d2e89e44d7f3a0ac8da727eba
varshini <dreamvarshini@gmail.com>
2021-09-19 21:04:31 +0530

    ADDED: main method to  call addressbook menu function

---
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBookMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.adressbooksystem;
+
+public class AddressBookMain 
+{
+
+	public static void main(String[] args)
+	{
+		
+		AddressBookMenu menu = new AddressBookMenu();
+		menu.performOperations();
+	}
+	
+}
