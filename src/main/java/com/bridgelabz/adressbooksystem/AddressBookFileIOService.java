b3ea8c2d4bc66be1e41da1d0f6525498ef71630f
varshini <dreamvarshini@gmail.com>
2021-09-21 01:28:52 +0530

    MODIFIED: function to read and write to csv file

--- src/main/java/com/bridgelabz/adressbooksystem/AddressBookFileIOService.java
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBookFileIOService.java
@@ -1,97 +1,206 @@
 package com.bridgelabz.adressbooksystem;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvException;
 
 public class AddressBookFileIOService 
 {
-	public static String ADDRESSBOOK_FILE_NAME="addressbook.txt";
+	public static String ADDRESSBOOK_TXT_FILE_NAME="addressbook.txt";
+	public static String ADDRESSBOOK_CSV_FILE_NAME="addressbook.csv";
+	public static String ADDRESSBOOK_JSON_FILE_NAME="addressbook.json";
+
+	public enum FileType {TXT,CSV,JSON}
 
 
-	public void writeData(List<Contact> contactList) 
+	public void writeData(List<Contact> contactList,FileType fileType,AddressBookSystem...addressBookSystem) throws IOException, CsvException 
 	{
-		StringBuffer contactBuffer = new StringBuffer();
-		contactList.forEach(contact -> {
-			String contactDataString =contact.toString().concat("\n");
-			contactBuffer.append(contactDataString);
-		});
-
-		try
+		if(fileType.equals(FileType.TXT))
 		{
-			Files.write(Paths.get(ADDRESSBOOK_FILE_NAME),contactBuffer.toString().getBytes());
-		}
-		catch (IOException e) 
-		{
-			e.printStackTrace();
-		}
+			StringBuffer contactBuffer = new StringBuffer();
+			contactList.forEach(contact -> {
+				String contactDataString =contact.toString().concat("\n");
+				contactBuffer.append(contactDataString);
+			});
 
+			try
+			{
+				Files.write(Paths.get(ADDRESSBOOK_TXT_FILE_NAME),contactBuffer.toString().getBytes());
+			}
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		else if(fileType.equals(FileType.CSV))
+		{
+			try
+			(Writer writer =Files.newBufferedWriter(Paths.get(ADDRESSBOOK_CSV_FILE_NAME))
+			)
+			{
+				StatefulBeanToCsv<Contact> beanToCsv= new StatefulBeanToCsvBuilder(writer)
+						.withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+						.build();
+
+				beanToCsv.write(contactList);
+			}
+			
+		}
 		
 	}
 	
-	public void printData() 
+	public void printData(FileType fileType) throws IOException, CsvException 
 	{
-		try 
+		if(fileType.equals(FileType.TXT))
 		{
-			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath())
-			.forEach(System.out::println);
-		} catch (Exception e) 
-		{
-			e.printStackTrace();
+			try 
+			{
+				Files.lines(new File(ADDRESSBOOK_TXT_FILE_NAME).toPath())
+				.forEach(System.out::println);
+			} catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
 		}
+		else if(fileType.equals(FileType.CSV))
+		{
+			
+			try
+			(
+				Reader reader =Files.newBufferedReader(Paths.get(ADDRESSBOOK_CSV_FILE_NAME));
+				CSVReader csvReader = new CSVReader(reader);
+			)
+			{
+				List<String[]> records = csvReader.readAll();
+				for(String[] record:records)
+				{
+					System.out.println("First Name: "+record[0]);
+					System.out.println("Last Name: "+record[1]);
+					System.out.println("Adddress: "+record[2]);
+					System.out.println("City: "+record[3]);
+					System.out.println("Zip: "+record[4]);
+					System.out.println("state: "+record[5]);
+					System.out.println("Phone number: "+record[6]);
+					System.out.println("Email: "+record[7]);
+
+					System.out.println("=========================");
+					
+				}
+			}
+		}
+		
+		
 	}
 	
-	public long countEntries()
+	public long countEntries(FileType fileType) throws IOException, CsvException
 	{
-		
-		long entries=0;
-		try {
-			entries=Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath())
-					.count();
-		}
-		catch (IOException e) 
+		if(fileType.equals(FileType.TXT))
 		{
-			e.printStackTrace();
+			long entries=0;
+			try {
+				entries=Files.lines(new File(ADDRESSBOOK_TXT_FILE_NAME).toPath())
+						.count();
+			}
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+			return entries;
 		}
-		return entries;
+		else if(fileType.equals(FileType.CSV))
+		{
+			return this.readData(FileType.CSV).size();
+		}
+		
+		return 0;
 	}
 
 	
-	public List<Contact> readData()
+	public List<Contact> readData(FileType fileType) throws IOException, CsvException 
 	{
 		List<Contact> contactList = new ArrayList<Contact>();
 		
 		List<String[]> contactDataFromFile = new ArrayList<String[]>();
-		try
+		if(fileType.equals(FileType.TXT))
 		{
-			Files.lines(new File(ADDRESSBOOK_FILE_NAME).toPath())
-				.map(line->line.trim())
-				.forEach(line->contactDataFromFile.add(line.split(",")));
-			
-			for(int index=0;index<contactDataFromFile.size();index++)
+			try
 			{
+				List<String[]> contactDataFromTXTFile = new ArrayList<String[]>();
+
+				Files.lines(new File(ADDRESSBOOK_TXT_FILE_NAME).toPath())
+					.map(line->line.trim())
+					.forEach(line->contactDataFromTXTFile.add(line.split(",")));
 				
-				contactList.add(new Contact(contactDataFromFile.get(index)[0].split("=")[1]
-										,contactDataFromFile.get(index)[1].split("=")[1]
-										,contactDataFromFile.get(index)[2].split("=")[1]
-										,contactDataFromFile.get(index)[3].split("=")[1]
-										,contactDataFromFile.get(index)[4].split("=")[1]
-										,contactDataFromFile.get(index)[5].split("=")[1]
-										,contactDataFromFile.get(index)[6].split("=")[1]
-										,contactDataFromFile.get(index)[7].split("=")[1]
-										));
+				
+				for(int index=0;index<contactDataFromTXTFile.size();index++)
+				{
+					
+					contactList.add(new Contact(contactDataFromTXTFile.get(index)[0].split("=")[1]
+											,contactDataFromTXTFile.get(index)[1].split("=")[1]
+											,contactDataFromTXTFile.get(index)[2].split("=")[1]
+											,contactDataFromTXTFile.get(index)[3].split("=")[1]
+											,contactDataFromTXTFile.get(index)[4].split("=")[1]
+											,contactDataFromTXTFile.get(index)[5].split("=")[1]
+											,contactDataFromTXTFile.get(index)[6].split("=")[1]
+											,contactDataFromTXTFile.get(index)[7].split("=")[1]
+											));
+				}
+				
+				
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else if(fileType.equals(FileType.CSV))
+		{
+			
+			try
+			(
+				Reader reader =Files.newBufferedReader(Paths.get(ADDRESSBOOK_CSV_FILE_NAME));
+				CSVReader csvReader = new CSVReader(reader);
+			)
+			{
+				 contactDataFromFile = csvReader.readAll();
+				 
+				 for(int index=1;index<contactDataFromFile.size();index++)
+					{
+						
+						contactList.add(new Contact(contactDataFromFile.get(index)[0]
+												,contactDataFromFile.get(index)[1]
+												,contactDataFromFile.get(index)[2]
+												,contactDataFromFile.get(index)[3]
+												,contactDataFromFile.get(index)[4]
+												,contactDataFromFile.get(index)[5]
+												,contactDataFromFile.get(index)[6]
+												,contactDataFromFile.get(index)[7]
+												));
+					}
 			}
 			
-			
 		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
-		
+	
 		return contactList;
 	}
+	
+	
+
 }
