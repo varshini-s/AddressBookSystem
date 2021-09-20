4c1f2987e4d5ee280c243cc264a4b870c41dca03
varshini <dreamvarshini@gmail.com>
2021-09-21 01:30:59 +0530

    MODIFIED: throws keyword for possible exceptions

--- src/main/java/com/bridgelabz/adressbooksystem/AddressBookMain.java
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBookMain.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.adressbooksystem;
 
+import java.io.IOException;
+
+import com.opencsv.exceptions.CsvException;
+
 public class AddressBookMain 
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException, CsvException
 	{
 		
 		AddressBookMenu menu = new AddressBookMenu();
