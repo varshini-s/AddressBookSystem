f7250c2f13709416ea1652bf63e43e3f3304f627
varshini <dreamvarshini@gmail.com>
2021-09-21 01:29:58 +0530

    ADDED: test case to check proper reading writing of csv file

--- src/test/java/com/bridgelabz/addressbooksystem/AddressBookFileIOServiceTest.java
+++ src/test/java/com/bridgelabz/addressbooksystem/AddressBookFileIOServiceTest.java
@@ -1,17 +1,24 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.io.IOException;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.bridgelabz.adressbooksystem.AddessBookServiceImpl;
 import com.bridgelabz.adressbooksystem.Contact;
+import com.opencsv.exceptions.CsvException;
 import com.bridgelabz.adressbooksystem.AddessBookServiceImpl.IOService;
+import com.bridgelabz.adressbooksystem.AddressBookFileIOService.FileType;
+import com.bridgelabz.adressbooksystem.AddressBookSystem;
 
 public class AddressBookFileIOServiceTest 
 
 {
 	AddessBookServiceImpl addressBookOperations;
+	AddressBookSystem addressBookSystem = new AddressBookSystem();
+
 	String addressBookName;
 
 	@Before
@@ -24,30 +31,53 @@ public class AddressBookFileIOServiceTest
 		addressBookOperations.addNewContact(new Contact("Sheldon","Cooper","texas","pasadena","california","11134","12345","abc@gmail.com"),addressBookName);
 		addressBookOperations.addNewContact(new Contact("Ross ","Geller","groov street","aaa","New york","12311","89897677","ross@gmail.com"),addressBookName);
 		addressBookOperations.addNewContact(new Contact("Ted","Mobsby","aaa","bbb","new york","44562","567890999","ted@gmail.com"),addressBookName);
+		
+		addressBookOperations.createAddressBook("Addressbook2");
+		addressBookName="Addressbook2";
+		addressBookOperations.addNewContact(new Contact("james","b","texas","pasadena","california","11134","12345","abc@gmail.com"),addressBookName);
+		addressBookOperations.addNewContact(new Contact("susan ","aaa","groov street","aaa","New york","12311","89897677","ross@gmail.com"),addressBookName);
+		addressBookOperations.addNewContact(new Contact("alan","c","aaa","bbb","new york","44562","567890999","ted@gmail.com"),addressBookName);	
 
 	}
 
 
 	@Test
-	public void given3ContactsWhenWrittenToFileShouldMatchContactEntries()
+	public void given3ContactsWhenWrittenToTXTFileShouldMatchContactEntries() throws IOException, CsvException
 	{
 
-		addressBookOperations.writeContactsOfAddressBook(IOService.FILE_IO,addressBookName);
-		addressBookOperations.printData(IOService.FILE_IO);
-
-		long entries = addressBookOperations.countEntries(IOService.FILE_IO);
-		Assert.assertEquals(3, entries);
+		addressBookOperations.writeContactsOfAddressBook(IOService.FILE_IO,addressBookName,FileType.TXT);
+		long txtEntries = addressBookOperations.countEntries(IOService.FILE_IO,FileType.TXT);
+		Assert.assertEquals(3, txtEntries);
 
 
 	}
+	@Test
+	public void given3ContactsWhenWrittenToCSVFileShouldMatchContactEntries() throws IOException, CsvException
+	{
+
+		addressBookOperations.writeContactsOfAddressBook(IOService.FILE_IO,addressBookName,FileType.CSV);
+		long csvEntries = addressBookOperations.countEntries(IOService.FILE_IO,FileType.CSV);
+		Assert.assertEquals(3, csvEntries);
+
+	}
+	
 
 	@Test
-	public void givenFileOnReadingFromFileShouldMatchContactsCount()
+	public void givenFileOnReadingFromTXTFileShouldMatchContactsCount() throws IOException, CsvException
 	{
-		long entries = addressBookOperations.readContactListData(IOService.FILE_IO,addressBookName);
-		Assert.assertEquals(3, entries);
-
+		
+		long txtEntries = addressBookOperations.readContactListData(IOService.FILE_IO,addressBookName,FileType.TXT);
+		Assert.assertEquals(3, txtEntries);
 
 	}
+	@Test
+	public void givenFileOnReadingFromCSVFileShouldMatchContactsCount() throws IOException, CsvException
+	{
+		
+		long csvEntries = addressBookOperations.readContactListData(IOService.FILE_IO,addressBookName,FileType.CSV);
+		Assert.assertEquals(3, csvEntries);
+
+	}
+
 
 }
