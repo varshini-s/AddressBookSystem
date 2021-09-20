56199f7a9316402385b1e069a6d0ea1fa28dc77f
varshini <dreamvarshini@gmail.com>
2021-09-21 01:21:04 +0530

    MODIFIED: passing print stream as parameter to function to test output

--- src/main/java/com/bridgelabz/adressbooksystem/AddressBookServiceIF.java
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBookServiceIF.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.adressbooksystem;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 
 import com.bridgelabz.adressbooksystem.AddessBookServiceImpl.IOService;
+import com.bridgelabz.adressbooksystem.AddressBookFileIOService.FileType;
+import com.opencsv.exceptions.CsvException;
 
 public interface AddressBookServiceIF 
 
@@ -12,24 +16,24 @@ public interface AddressBookServiceIF
 	public void deleteContact(String number,String  addressbookName);
 	public int hasContact(String number,String  addressbookName);
 	public void displayContactInfo(String number,String  addressbookName);
-	public void searchPersonByState(String givenName,String state); 
-	public void searchPersonByCity(String name,String city);
-	public void getAllContactsInState(String state);
-	public void getAllContactsInCity(String city);
-	public void countPeopleinCity(String city);
-	public void countPeopleinState(String state);
-	public void sortByName();
-	public void sortByCity();
-	public void sortByState();
-	public void sortByZip();
+	public void searchPersonByState(String givenName,String state,PrintStream... printStreamObject);
+	public void searchPersonByCity(String givenName,String city,PrintStream... printStreamObject);  
+	public void getAllContactsInState(String state,PrintStream... printStreamObject);
+	public void getAllContactsInCity(String city,PrintStream... printStreamObject);
+	public void countPeopleinCity(String city,PrintStream... printStreamObject);
+	public void countPeopleinState(String state,PrintStream... printStreamObject);
+	public void sortByName(PrintStream... printStreamObject);
+	public void sortByCity(PrintStream... printStreamObject);
+	public void sortByState(PrintStream... printStreamObject);
+	public void sortByZip(PrintStream... printStreamObject);
 	
 	public void createAddressBook(String addressbookName);
 	public AddressBook getAddressBook(String addressbookName);
 	
 	public List<Contact> getContactList(String  addressbookName);
-	public void writeContactsOfAddressBook(IOService ioService,String  addressbookName);
-	public void printData(IOService ioService);
-	public long countEntries(IOService ioService); 
-	public long readContactListData(IOService ioService,String  addressbookName);
+	public void writeContactsOfAddressBook(IOService ioService,String  addressbookName,FileType... fileType) throws IOException, CsvException;
+	public void printData(IOService ioService,FileType... fileType) throws IOException, CsvException;
+	public long countEntries(IOService ioService,FileType... fileType) throws IOException, CsvException;
+	public long readContactListData(IOService ioService,String  addressbookName,FileType... fileType) throws IOException, CsvException;
 
 }
