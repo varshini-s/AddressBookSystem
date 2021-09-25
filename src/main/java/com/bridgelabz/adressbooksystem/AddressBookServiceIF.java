f0598ea5de095e1ee259f9dfc20e4bfa102ae478
varshini <dreamvarshini@gmail.com>
2021-09-26 02:48:38 +0530

    DELETED: seperating IO operation from basic operations

--- src/main/java/com/bridgelabz/adressbooksystem/AddressBookServiceIF.java
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBookServiceIF.java
@@ -27,13 +27,6 @@ public interface AddressBookServiceIF
 	public void sortByState(PrintStream... printStreamObject);
 	public void sortByZip(PrintStream... printStreamObject);
 	
-	public void createAddressBook(String addressbookName);
-	public AddressBook getAddressBook(String addressbookName);
 	
-	public List<Contact> getContactList(String  addressbookName);
-	public void writeContactsOfAddressBook(IOService ioService,String  addressbookName,FileType... fileType) throws IOException, CsvException;
-	public void printData(IOService ioService,FileType... fileType) throws IOException, CsvException;
-	public long countEntries(IOService ioService,FileType... fileType) throws IOException, CsvException;
-	public List<Contact> readContactListData(IOService ioService,String  addressbookName,FileType... fileType) throws IOException, CsvException;
 
 }
