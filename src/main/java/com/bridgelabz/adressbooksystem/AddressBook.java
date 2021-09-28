5c29d8329be4aa96ac178f78e711632241de9b7f
varshini <dreamvarshini@gmail.com>
2021-09-29 00:54:16 +0530

    REFACTORED: addressbook class to match database structure

--- src/main/java/com/bridgelabz/adressbooksystem/AddressBook.java
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBook.java
@@ -9,52 +9,66 @@ public class AddressBook
 {
 	private String AddressBookName;
 	private List<Contact> contactList = new LinkedList<Contact>();
+	private AddressBookType addressBookType;
 	private transient HashMap<String, List<Contact>> cityDictionary = new HashMap<String, List<Contact>>();
 	private transient HashMap<String, List<Contact>> stateDictionary = new HashMap<String, List<Contact>>();
 
 
-	public HashMap<String, List<Contact>> getCityDictionary() {
+	public HashMap<String, List<Contact>> getCityDictionary()
+	{
 		return cityDictionary;
 	}
 
-	public void setCityDictionary(HashMap<String, List<Contact>> cityDictionary) {
+	public void setCityDictionary(HashMap<String, List<Contact>> cityDictionary)
+	{
 		this.cityDictionary = cityDictionary;
 	}
 
-	public HashMap<String, List<Contact>> getStateDictionary() {
+	public HashMap<String, List<Contact>> getStateDictionary() 
+	{
 		return stateDictionary;
 	}
 
-	public void setStateDictionary(HashMap<String, List<Contact>> stateDictionary) {
+	public void setStateDictionary(HashMap<String, List<Contact>> stateDictionary) 
+	{
 		this.stateDictionary = stateDictionary;
 	}
 
 
+	public String getAddressBookName()
+	{
+		return AddressBookName;
 
-	
+	}
 
-	 public String getAddressBookName()
-	 {
-		 return AddressBookName;
-		 
-	 }
-	
-	
-	 
-	 public void setAddressBookName(String AddressBookName)
-	 {
-		 this.AddressBookName=AddressBookName;
-		 
-	 }
-	 public List<Contact> getContactList() {
-			return contactList;
-		}
 
-		public void setContactList(List<Contact> newlist) {
-			this.contactList = newlist;
-		}
-	
-	
 
-	
+	public void setAddressBookName(String AddressBookName)
+	{
+		this.AddressBookName=AddressBookName;
+
+	}
+	public List<Contact> getContactList()
+	{
+		return contactList;
+	}
+
+	public void setContactList(List<Contact> newlist) 
+	{
+		this.contactList = newlist;
+	}
+
+	public AddressBookType getAddressBookType()
+	{
+		return addressBookType;
+	}
+
+	public void setAddressBookType(AddressBookType addressBookType)
+	{
+		this.addressBookType = addressBookType;
+	}
+
+
+
+
 }
