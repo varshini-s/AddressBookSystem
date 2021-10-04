5b17c15ed84f6d181007997d1864c03f420d5eff
varshini <dreamvarshini@gmail.com>
2021-10-04 14:57:34 +0530

    ADDED: function to add new contact and its address to database

--- src/main/java/com/bridgelabz/adressbooksystem/AddressBookDBService.java
+++ src/main/java/com/bridgelabz/adressbooksystem/AddressBookDBService.java
@@ -323,6 +323,188 @@ public class AddressBookDBService implements Runnable
 		return 0;
 
 	}
+	
+	public  synchronized ContactDTO addContact(Contact givenContact,String  addressBookName,LocalDate dateAdded)
+	{
+		int contactId=-1;
+		Connection connection=null;
+		ContactDTO contact=null;
+		String firstName=givenContact.getFirstName();
+		String lastName=givenContact.getLastName();
+		String email=givenContact.getEmail();
+		String phoneNumber=givenContact.getPhoneNumber();
+		int addressBookID = this.getAddressBookID( addressBookName);
+		String houseNumber=givenContact.getContactAddress().getHouseNumber();
+		String street=givenContact.getContactAddress().getStreet();
+		String city=givenContact.getContactAddress().getCity();
+		String state=givenContact.getContactAddress().getState();
+		String zip=givenContact.getContactAddress().getZip();
+		
+		if(checkContactExists(firstName,lastName,email,phoneNumber,addressBookID)==false)
+		{
+			
+			try 
+			{
+				connection=this.getConnection();
+				connection.setAutoCommit(false);
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+
+			contactId = insertContactToDatabase(firstName, lastName, phoneNumber, email, addressBookID, contactId,
+					connection,dateAdded);
+
+
+			contact = insertAddressToDataBase(firstName, lastName, houseNumber, street, city, state, zip, phoneNumber,
+					email, contactId, connection, contact);
+			try 
+			{
+				connection.commit();
+
+			} catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				if(connection!=null)
+				{
+					try 
+					{
+						connection.close();
+
+					} catch (Exception e) 
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		else 
+		{
+			System.out.println("duplicate contact");
+		}
+		
+
+		
+		return contact;
+	}
+
+	private ContactDTO insertAddressToDataBase(String firstName, String lastName, String houseNumber, String street,
+			String city, String state, String zip, String phoneNumber, String email, int contactId,
+			Connection connection, ContactDTO contact) {
+		try(Statement statement=connection.createStatement())
+		{
+			String sql=String.format("Insert into address values "
+					+ "('%d','%s','%s','%s','%s','%s');",contactId,houseNumber,street,city,state,zip);
+			int rowAffected = statement.executeUpdate(sql,statement.RETURN_GENERATED_KEYS);
+			if(rowAffected==1)
+			{
+				ResultSet resultSet=statement.getGeneratedKeys();
+				if(resultSet.next()) contactId=resultSet.getInt(1);
+				Address address= new Address(houseNumber, street, city, state, zip);
+				contact = new ContactDTO(contactId,firstName,lastName,address,phoneNumber,email);
+
+			}
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			try 
+			{
+				connection.rollback();
+
+			} catch (Exception ex) 
+			{
+				ex.printStackTrace();
+			}
+		}
+		return contact;
+	}
+
+	private int insertContactToDatabase(String firstName, String lastName, String phoneNumber, String email,
+			int addressBookId, int contactId, Connection connection,LocalDate dateAdded) {
+		try(Statement statement=connection.createStatement())
+		{
+			String sql=String.format("INSERT INTO contact(firstName,lastName,phoneNumber,email,address_book_id,date_added) "
+					+ "values ('%s','%s','%s','%s','%s','%s');",firstName,lastName,phoneNumber,email,addressBookId,dateAdded);
+			int rowAffected = statement.executeUpdate(sql,statement.RETURN_GENERATED_KEYS);
+			if(rowAffected==1)
+			{
+				ResultSet resultSet=statement.getGeneratedKeys();
+				if(resultSet.next()) contactId=resultSet.getInt(1);
+			}
+
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+			try 
+			{
+				connection.rollback();
+
+			} catch (Exception ex) 
+			{
+
+				ex.printStackTrace();
+			}
+		}
+		return contactId;
+	}
+
+	private int getAddressBookID(String addressBookName) 
+	{
+		int id=0;
+		String sql = String.format("Select address_book_id  from address_book "
+								+ "where address_book_name=\"%s\";", addressBookName);
+		try (Connection connection = this.getConnection())
+		{
+
+			Statement statement=connection.createStatement();
+			ResultSet resultSet= statement.executeQuery(sql);
+			while(resultSet.next())
+			{
+				 id=resultSet.getInt("address_book_id");
+			}
+			
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return id;
+	
+	}
+
+	private boolean checkContactExists(String firstName, String lastName, String email, String phoneNumber, int addressBookId) 
+	{
+		String sql=String.format("select address_book_id from contact where "+"firstName ='%s' and lastName='%s' and email='%s' and phoneNumber='%s';",
+									firstName,lastName,email,phoneNumber);
+		
+		try (Connection connection = this.getConnection())
+		{
+
+			Statement statement=connection.createStatement();
+			ResultSet resultSet= statement.executeQuery(sql);
+			while(resultSet.next())
+			{
+				int id=resultSet.getInt("address_book_id");
+				if(id==addressBookId)
+				{
+					return true;
+
+				}
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
 
 }
 
